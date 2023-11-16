d797773ccf6593cb38c9c315b727642bf30ec95b
Jean Thirion <JeanThirion@ssw.com.au>
2023-11-17 09:22:26 +1100

    Change detection strategy demo

--- firebootcamp-crm/src/app/app.module.ts
+++ firebootcamp-crm/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CompanyListComponent } from './company/company-list/company-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
 import { CompanyTableComponent } from './company/company-table/company-table.component';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 
--- firebootcamp-crm/src/app/company/company-table/company-table.component.ts
+++ firebootcamp-crm/src/app/company/company-table/company-table.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Company } from '../company';
 
 @Component({
   selector: 'fbc-company-table',
   templateUrl: './company-table.component.html',
-  styleUrl: './company-table.component.scss'
+  styleUrl: './company-table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CompanyTableComponent {
   @Input()
@@ -16,5 +17,9 @@ export class CompanyTableComponent {
   deleteCompany(company: Company) {
     console.log("Delete works in child component", company)
     this.deleteClickedEvent.emit(company);
+  }
+
+  logChanges() {
+    console.log("REPAINTING COMPONENT");
   }
 }
